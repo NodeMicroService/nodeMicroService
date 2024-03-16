f303e7bbc4c7cda341c39428cd6485d00a157383
AssassinLYZ <goatherd.eric@gmail.com>
2024-03-16 14:16:45 +0100

    Feat:Create Structure for microService

---
+++ shopping/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use("/", (req, res, next) => {
+  return res.status(200).json({ msg: "Hello, Shopping" });
+});
+app.listen(8003, () => {
+  console.log("Shopping is listening on port 8003");
+});
